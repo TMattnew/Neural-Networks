bf503888ab7094afbbd1fd6175892ec354073dd2
TMattnew <71871149+TMattnew@users.noreply.github.com>
2021-04-26 12:04:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from random import *
 import numpy as np
 from math import *
+import matplotlib.pyplot as plt
 
 
-#  rand  -->    [[],
-#                []],      []
 def data_generator():
     a = random() * 10
     b = random() * 10
@@ -13,19 +12,18 @@ def data_generator():
     return d
 
 
-#     [] --> []
 def error(expected, real):
     return (expected - real)**2
 
 
-#     [] --> []
+def demonstration_error(expected, real):
+    return expected - real
+
+
 def error_derivative(expected, real):
     return 2*(real - expected)
 
 
-#     [[],         [[],
-#      [],   -->    [],
-#      []]          []]
 def ReLU(v):
     res = []
     s = v.shape
@@ -34,9 +32,6 @@ def ReLU(v):
     return np.array(res).reshape(s)
 
 
-#     [[],         [[], 0, 0
-#      [],   -->    0, [], 0
-#      []]          0, 0, []]
 def ReLU_der(v):
     res = []
     for it in v:
@@ -49,87 +44,167 @@ def ReLU_der(v):
 
 
 
-# [[ , ],
-#  [ , ]]
-first_layer_matrix = np.random.rand(16, 2)
 
-# [[],
-#  []]
-first_bias_matrix = np.random.rand(16, 1)
+first_layer_matrix = np.random.rand(16, 2) - 0.5 * np.ones((16, 2))
 
-# [[ , ],
-#  [ , ]]
-second_layer_matrix = np.random.rand(8, 16)
-
-# [[],
-#  []]
-second_bias_matrix = np.random.rand(8, 1)
-
-# [[ , ]]
-third_layer_matrix = np.random.rand(1, 8)
+first_bias_matrix = np.random.rand(16, 1) - 0.5 * np.ones((16, 1))
 
 
-# [[]]
-third_bias_matrix = np.random.rand(1, 1)
+second_layer_matrix = np.random.rand(16, 16) - 0.5 * np.ones((16, 16))
+
+second_bias_matrix = np.random.rand(16, 1) - 0.5 * np.ones((16, 1))
 
 
+third_layer_matrix = np.random.rand(8, 16) - 0.5 * np.ones((8, 16))
 
-L = 0.0001  # LEARNING RATE
-
-
-for i in range(100000):
-    data = data_generator()
-
-    # [[],
-    #  []]
-    input_vector = data[0]
-
-    # scalar
-    expected_output = data[1]
+third_bias_matrix = np.random.rand(8, 1) - 0.5 * np.ones((8, 1))
 
 
-    # [[],
-    #  []]
-    first_layer_output_UR = np.matmul(first_layer_matrix, input_vector) + first_bias_matrix
+fourth_layer_matrix = np.random.rand(8, 8) - 0.5 * np.ones((8, 8))
 
-    # [[],
-    #  []]
-    first_layer_output = ReLU(first_layer_output_UR)
-
-    # [[],
-    #  []]
-    second_layer_output_UR = np.matmul(second_layer_matrix, first_layer_output) + second_bias_matrix
+fourth_bias_matrix = np.random.rand(8, 1) - 0.5 * np.ones((8, 1))
 
 
-    # [[],
-    #  []]
-    second_layer_output = ReLU(second_layer_output_UR)
-
-
-    # []
-    third_layer_output = np.matmul(third_layer_matrix, second_layer_output) + third_bias_matrix
+fith_layer_matrix = np.random.rand(1, 8) - 0.5 * np.ones((1, 8))
+fith_bias_matrix = np.random.rand(1, 1) - 0.5 * np.ones((1, 1))
 
 
 
-    E_der = error_derivative(expected_output, third_layer_output)
 
-    delta_3 = E_der
-    delta_2 = np.matmul(delta_3, np.matmul(third_layer_matrix, ReLU_der(second_layer_output_UR)))
-    delta_1 = np.matmul(delta_2, np.matmul(second_layer_matrix, ReLU_der(first_layer_output_UR)))
+result = []
+L = 0.0000005  # LEARNING RATE
+FRIC = 0.7  # 1 - Friction coefficient
+
+prev_first_matrix_grad = 0
+prev_first_bias_grad = 0
+prev_second_matrix_grad = 0
+prev_second_bias_grad = 0
+prev_third_matrix_grad = 0
+prev_third_bias_grad = 0
+prev_fourth_matrix_grad = 0
+prev_fourth_bias_grad = 0
+prev_fith_matrix_grad = 0
+prev_fith_bias_grad = 0
 
 
-    third_bias_matrix -= L * delta_3.transpose()
-    third_layer_matrix -= L * np.matmul(delta_3.transpose(), second_layer_output.transpose())
+for i in range(1000000):
+    first_matrix_grad = 0
+    first_bias_grad = 0
+    second_matrix_grad = 0
+    second_bias_grad = 0
+    third_matrix_grad = 0
+    third_bias_grad = 0
+    fourth_matrix_grad = 0
+    fourth_bias_grad = 0
+    fith_matrix_grad = 0
+    fith_bias_grad = 0
 
-    second_bias_matrix -= L * delta_2.transpose()
-    second_layer_matrix -= L * np.matmul(delta_2.transpose(), first_layer_output.transpose())
 
-    first_bias_matrix -= L * delta_1.transpose()
-    first_layer_matrix -= L * np.matmul(delta_1.transpose(), input_vector.transpose())
+    for j in range(1):
+        data = data_generator()
 
+        input_vector = data[0]
+        expected_output = data[1]
+
+
+        first_layer_output_UR = np.matmul(first_layer_matrix, input_vector) + first_bias_matrix
+
+        first_layer_output = ReLU(first_layer_output_UR)
+
+
+        second_layer_output_UR = np.matmul(second_layer_matrix, first_layer_output) + second_bias_matrix
+
+        second_layer_output = ReLU(second_layer_output_UR)
+
+
+        third_layer_output_UR = np.matmul(third_layer_matrix, second_layer_output) + third_bias_matrix
+
+        third_layer_output = ReLU(third_layer_output_UR)
+
+
+        fourth_layer_output_UR = np.matmul(fourth_layer_matrix, third_layer_output) + fourth_bias_matrix
+
+        fourth_layer_output = ReLU(fourth_layer_output_UR)
+
+
+        fith_layer_output = np.matmul(fith_layer_matrix, fourth_layer_output) + fith_bias_matrix
+
+
+
+        E_der = error_derivative(expected_output, fith_layer_output)
+
+        delta_5 = E_der
+        delta_4 = np.matmul(delta_5, np.matmul(fith_layer_matrix, ReLU_der(fourth_layer_output_UR)))
+        delta_3 = np.matmul(delta_4, np.matmul(fourth_layer_matrix, ReLU_der(third_layer_output_UR)))
+        delta_2 = np.matmul(delta_3, np.matmul(third_layer_matrix, ReLU_der(second_layer_output_UR)))
+        delta_1 = np.matmul(delta_2, np.matmul(second_layer_matrix, ReLU_der(first_layer_output_UR)))
+
+
+        first_matrix_grad += L * np.matmul(delta_1.transpose(), input_vector.transpose())
+        first_bias_grad += L * delta_1.transpose()
+        second_matrix_grad += L * np.matmul(delta_2.transpose(), first_layer_output.transpose())
+        second_bias_grad += L * delta_2.transpose()
+        third_matrix_grad += L * np.matmul(delta_3.transpose(), second_layer_output.transpose())
+        third_bias_grad += L * delta_3.transpose()
+        fourth_matrix_grad += L * np.matmul(delta_4.transpose(), third_layer_output.transpose())
+        fourth_bias_grad += L * delta_4.transpose()
+        fith_matrix_grad += L * np.matmul(delta_5.transpose(), fourth_layer_output.transpose())
+        fith_bias_grad += L * delta_5.transpose()
+
+        first_matrix_grad += FRIC * prev_first_matrix_grad
+        first_bias_grad += FRIC * prev_first_bias_grad
+        second_matrix_grad += FRIC * prev_second_matrix_grad
+        second_bias_grad += FRIC * prev_second_bias_grad
+        third_matrix_grad += FRIC * prev_third_matrix_grad
+        third_bias_grad += FRIC * prev_third_bias_grad
+        fourth_matrix_grad += FRIC * prev_fourth_matrix_grad
+        fourth_bias_grad += FRIC * prev_fourth_bias_grad
+        fith_matrix_grad += FRIC * prev_fith_matrix_grad
+        fith_bias_grad += FRIC * prev_fith_bias_grad
+
+        if j == 0:
+            print(demonstration_error(expected_output, fith_layer_output), "\n    <-    \n", input_vector, "\n\n")
+        if i % 1 == 0:
+            result.append(demonstration_error(expected_output, fith_layer_output)[0][0])
+
+
+
+
+
+    fith_layer_matrix -= fith_matrix_grad
+    fith_bias_matrix -= fith_bias_grad
+
+    fourth_layer_matrix -= fourth_matrix_grad
+    fourth_bias_matrix -= fourth_bias_grad
+
+    third_layer_matrix -= third_matrix_grad
+    third_bias_matrix -= third_bias_grad
+
+
+    second_layer_matrix -= second_matrix_grad
+    second_bias_matrix -= second_bias_grad
+
+
+    first_layer_matrix -= first_matrix_grad
+    first_bias_matrix -= first_bias_grad
+
+    prev_first_matrix_grad = first_matrix_grad
+    prev_first_bias_grad = first_bias_grad
+    prev_second_matrix_grad = second_matrix_grad
+    prev_second_bias_grad = second_bias_grad
+    prev_third_matrix_grad = third_matrix_grad
+    prev_third_bias_grad = third_bias_grad
+    prev_fourth_matrix_grad = fourth_matrix_grad
+    prev_fourth_bias_grad = fourth_bias_grad
+    prev_fith_matrix_grad = fith_matrix_grad
+    prev_fith_bias_grad = fith_bias_grad
     # print(error_derivative(expected_output, third_layer_output))
-    print(expected_output, " -> ", third_layer_output)
     # print(error(expected_output, third_layer_output))
 
+plt.plot(result)
+plt.show()
 
+
+# with open("results.txt", "w") as f:
+#     f.write(str(result))
 
